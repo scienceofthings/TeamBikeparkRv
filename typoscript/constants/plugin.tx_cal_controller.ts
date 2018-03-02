a0c6625ce7b38fdeba06fe589f70ed99ee301346
Gregor Hyneck <supergreg@gmx.de>
2018-03-02 13:17:15 +0100

    Change calendar from list to month view

--- typoscript/constants/plugin.tx_cal_controller.ts
+++ typoscript/constants/plugin.tx_cal_controller.ts
@@ -1,16 +1,6 @@
-plugin.tx_cal_controller.rights.edit = 1
-plugin.tx_cal_controller.rights.admin.group = 1
-plugin.tx_cal_controller.pidList = 5
-plugin.tx_cal_controller.rights.defaultSavePid = 5
-plugin.tx_cal_controller.view.timeFormat = %H:%M
-plugin.tx_cal_controller.view.dateFormat = %d. %B
-
-plugin.tx_cal_controller.view.list.listTemplate = fileadmin/default/templates/extensions/cal/list.tmpl
-plugin.tx_cal_controller.view.event.eventModelTemplate = fileadmin/default/templates/extensions/cal/event_model.tmpl
-plugin.tx_cal_controller.view.month.horizontalSidebarTemplate = fileadmin/default/templates/extensions/cal/calendar_nav.tmpl
-
-
-[PIDinRootline=10]
-plugin.tx_cal_controller.view.event.eventModelTemplate = fileadmin/default/templates/extensions/cal/ListFull/event_model.tmpl
-plugin.tx_cal_controller.view.list.listTemplate = fileadmin/default/templates/extensions/cal/ListFull/list.tmpl
-[end]
+plugin.tx_cal_controller{
+    pidList = 5
+    view.timeFormat = %H:%M
+    view.dateFormat = %d. %B
+    view.month.monthTemplate = fileadmin/default/templates/extensions/cal/month.tmpl
+}
